cbc77463bef8d81d022fbb7dc5d4bde8a79877aa
Omar Roth <omarroth@protonmail.com>
2020-07-30 08:27:42 -0400

    Revert "Fix JSON response"
    This reverts commit 1df56ebcb808470ad3a5ec300d436852caa61f2f.

--- src/instances.cr
+++ src/instances.cr
@@ -24,7 +24,7 @@ macro rendered(filename)
   render "src/instances/views/#{{{filename}}}.ecr"
 end
 
-alias Instance = {flag: String?, region: String?, stats: JSON::Any?, type: String, uri: String, monitor: JSON::Any?}
+alias Instance = NamedTuple(flag: String?, region: String?, stats: JSON::Any?, type: String, uri: String, monitor: JSON::Any?)
 
 INSTANCES = {} of String => Instance
 
@@ -114,10 +114,7 @@ get "/instances.json" do |env|
   sort_by = env.params.query["sort_by"]?
   sort_by ||= "type,users"
 
-  instances = sort_instances(INSTANCES, sort_by).map do |instance|
-    name, value = instance
-    Hash{name => value}
-  end
+  instances = sort_instances(INSTANCES, sort_by)
 
   if env.params.query["pretty"]?.try &.== "1"
     instances.to_pretty_json
